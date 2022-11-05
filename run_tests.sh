cb62b6c8149979321369a5b2722447c33d22bc3e
lucasheld <lucasheld@hotmail.de>
2022-11-05 18:48:59 +0100

    test: adjust uptime kuma versions

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-for version in 1.17.0 1.17.1 1.18.0 1.18.2
+for version in 1.18.3 1.17.1
 do
   echo "Starting uptime kuma $version..."
   docker run -d -it --rm -p 3001:3001 --name uptimekuma "louislam/uptime-kuma:$version" > /dev/null
 
   while [[ "$(curl -s -L -o /dev/null -w ''%{http_code}'' 127.0.0.1:3001)" != "200" ]]
   do
-    sleep 1
+    sleep 0.5
   done
 
   echo "Running tests..."
